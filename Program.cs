1f514c95c967a8ad8783903fb66cfa7e0518815c
Aaron Miller <millerac333@gmail.com>
2018-08-27 20:26:05 -0500

    added a dictionarty containig family members

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿//  1. Define a Dictionary that contains information about several members of your family.
-
-//  2. Next, iterate over each item in myFamily and produce the following output.
+﻿//  2. Next, iterate over each item in myFamily and produce the following output.
 
 using System;
+using System.Collections.Generic;
 
 namespace family_dictionary
 {
@@ -10,7 +9,18 @@ namespace family_dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Define a Dictionary that contains information about several members of your family.
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Cindy" }, { "age", "56" } });
+
+            myFamily.Add("father", new Dictionary<string, string>() { { "name", "Joseph" }, { "age", "60" } });
+
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Austin" }, { "age", "32" } });
+
+            myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Shane" }, { "age", "28" } });
+
+            myFamily.Add("me", new Dictionary<string, string>() { { "name", "Aaron" }, { "age", "30" } });
         }
     }
 }
