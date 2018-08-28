e324f36b6e106c6510b1e8d4cecad0a617134c2f
Aaron Miller <millerac333@gmail.com>
2018-08-27 20:18:00 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//  1. Define a Dictionary that contains information about several members of your family.
+
+//  2. Next, iterate over each item in myFamily and produce the following output.
+
+using System;
+
+namespace family_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
